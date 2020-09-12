ff6f2d31faaaafc82d6020f3c20754d6ce71b8e8
ferris1 <1240990785@qq.com>
2020-09-12 12:48:18 +0800

    commit server framework

--- engine/serverbase.go
+++ engine/serverbase.go
@@ -1,1 +1,26 @@
 package engine
+
+type ServerBase struct {
+	ServerName string
+	ServerId   string
+}
+
+//启动之前的设置
+func (s *ServerBase) SetUp() {
+
+}
+
+// 注册消息
+func (s *ServerBase) Register() {
+
+}
+
+// 启动服务器
+func (s *ServerBase) StartService() {
+
+}
+
+// 退出服务器
+func (s *ServerBase) ExitService() {
+
+}
