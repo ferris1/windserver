a8bb976f73d0b40d874c3a566a1b36be95142d27
ferris1 <1240990785@qq.com>
2020-11-08 12:46:35 +0800

    add vendor dir and mod file

--- engine/engineBase.go
+++ engine/engineBase.go
@@ -8,6 +8,7 @@ type EngineBase struct {
 	ServerType 				int
 	ConnectCount			int
 	TotalConnectCount		int
+	serverExited			bool
 	// player 请求信息发送
 	requestQueue 			utilize.EsQueue
 	// 消息回调函数注册 应该是个回调函数
@@ -28,18 +29,27 @@ func (s *EngineBase) SetUp() {
 }
 
 // RPC框架  这个实现上好像比较麻烦
+// 事件注册:如网络事件注册
 func (s *EngineBase) Register() {
 
 
 }
 
-// 启动服务器
+// 启动服务器,一些服务线程将在这里启动,一些定时任务在这里驱动
 func (s *EngineBase) StartService() {
 	// 到etcd中注册服务器信息
 	// 启动消息处理线程
+	go s.handleRequestQueue()
 }
 
 // 退出服务器
 func (s *EngineBase) ExitService() {
 
 }
+
+func (s *EngineBase) handleRequestQueue() {
+	for s.serverExited {
+
+	}
+}
+
